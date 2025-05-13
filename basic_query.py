15cf453a1343c8710c9846662dd3fc0ac9d6fefb
Adel Siddiquei <adelsiddiquei@gmail.com>
2025-05-13 12:05:43 +0100

    updated .gitignore to ignore csv files, updated basic_query.py to use a .env file

--- basic_query.py
+++ basic_query.py
@@ -1,12 +1,25 @@
+import os
+from dotenv import load_dotenv
 import psycopg
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Access the variables
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+
 
 # Connect to the PostgreSQL database
 conn = psycopg.connect(
-    dbname="coffee_sales",
-    user="postgres",
-    password="postgres",
-    host="localhost",
-    port=5432
+    dbname=DB_NAME,
+    user= DB_USER,
+    password=DB_PASSWORD,
+    host=DB_HOST,
+    port=DB_PORT,
 )
 
 # Create a cursor
